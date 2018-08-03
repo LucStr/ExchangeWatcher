8baba2932799be4f41db2f0822f06d5b4d84bf71
Luca Strebel <luca.strebel@gmx.net>
2018-08-03 23:17:38 +0200

    add mustache js

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,32 @@
-console.error('APPP');
+/*console.error('APPP');
 
 import { exchanges } from 'ccxt';
 
 import * as ccxt from 'ccxt';
 
 console.log(exchanges.length, ccxt.exchanges.length);
-
+*/
 
 import * as express from 'express';
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+import * as mustacheExpress from 'mustache-express';
+declare var __dirname;
+let viewFolder = __dirname + '/../views';
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// Register '.mustache' extension with The Mustache Express
+app.engine('mustache', mustacheExpress(viewFolder, '.mustache'));
+
+app.set('view engine', 'mustache');
+app.set('views', __dirname + '/../views');
+app.set("layout", "layout");
+
+
+app.get('/', (req, res) => res.render('test', {name: 'Luca'})
+
+//.send('Hello World!')
+);
+
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
